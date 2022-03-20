cbf97d12578b4473c8290c84e7c19c68bb56b095
jueun275 <52851721+jueun275@users.noreply.github.com>
2022-03-20 18:30:27 +0900

    공공데이터api 테스트 코드

---
+++ src/test/java/com/solemate/endpoint/openapi/OpenApiControllerTest.java
@@ -0,0 +1,55 @@
+package com.solemate.endpoint.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OpenApiControllerTest {
+
+    @LocalServerPort
+    private  int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 측정소정보_가져오다(){
+        double currLat = 37.5666805;
+        double currLon = 126.9784147;
+
+        String url = "http://localhost:" + port + "/openApi/"+currLat+"/"+currLon;
+
+        //when
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        System.out.print(responseEntity);
+    }
+
+    @Test
+    public void 날씨정보_가져오다( ){
+        double currLat = 37.5666805;
+        double currLon = 126.9784147;
+
+        String url = "http://localhost:" + port + "/openApi/weather/"+currLat+"/"+currLon;
+
+        //when
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        System.out.print(responseEntity);
+    }
+}
